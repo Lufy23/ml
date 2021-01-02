d6bcbfdb4749cd195420b2a2b5a779695bc606b3
“Lufy23” <anveshkonda5@gmail.com>
2021-01-02 07:06:31 +0530

    test

---
+++ sample.py
@@ -0,0 +1,75 @@
+from sklearn.preprocessing import LabelEncoder
+
+# Make copy to avoid changing original data 
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+# Apply label encoder to each column with categorical data
+label_encoder = LabelEncoder()
+for col in object_cols:
+    label_X_train[col] = label_encoder.fit_transform(X_train[col])
+    label_X_valid[col] = label_encoder.transform(X_valid[col])
+
+print("MAE from Approach 2 (Label Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+
+
+
+
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+
+
+
+## -- Model Validation --
+# Splitting data into validation and train parts
+
+from sklearn.model_selection import train_test_split
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+
+
+
+
+# categorical data encoding 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
